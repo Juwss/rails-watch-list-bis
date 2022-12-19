bbcbca94e68f463f0a88b9df3a110380abeb1566
Julia Weiss <juliaweiss@live.fr>
2022-12-19 19:24:27 +0100

    seeds and lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :lists, except: [:destroy, :edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
   # Defines the root path route ("/")
   # root "articles#index"
 end
